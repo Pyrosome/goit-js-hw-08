359c260542710ff90b9f3a04878f709c053f31ee
Gaultheria <valeriaburina@gmail.com>
2023-01-12 22:14:49 +0100

    task 1 gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
-// Add imports above this line
+// Выполняй это задание в файлах 01-gallery.html и 01-gallery.js. Разбей его на несколько подзадач:
+
+// Добавь библиотеку SimpleLightbox https://simplelightbox.com/ как зависимость проекта используя npm (ссылка на CDN из твоей прошлой работы больше не нужна).
+// Используй свой JavaScript код из предыдущей домашней работы, но выполни рефакторинг с учетом того, что библиотека была установлена через npm (синтаксис import/export).
+// Для того чтобы подключить CSS код библиотеки в проект, необходимо добавить еще один импорт, кроме того который описан в документации.
+
+// // Описан в документации
+// import SimpleLightbox from "simplelightbox";
+// // Дополнительный импорт стилей
+// import "simplelightbox/dist/simple-lightbox.min.css";
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import SimpleLightbox from 'simplelightbox';
 import { galleryItems } from './gallery-items';
+
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const markup = galleryItems.reduce(
+  (acc, { preview, original, description }) =>
+    acc +
+    `<a class="gallery__item" href="${original}">
+      <img class="gallery__image"
+      src="${preview}"
+      alt="${description}" />
+    </a>`,
+  ''
+);
+gallery.insertAdjacentHTML('beforeend', markup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
