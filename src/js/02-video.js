fdf5c1b4b5f4ea9c36c33b316ad3d82169193017
Gaultheria <valeriaburina@gmail.com>
2023-01-14 17:57:41 +0100

    task 2 video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const time = 'videoplayer-current-time';
+
+timeset();
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay({ seconds }) {
+  localStorage.setItem(time, seconds);
+}
+
+function timeset() {
+  if (!localStorage.getItem(time)) {
+    return;
+  }
+  player.setCurrentTime(localStorage.getItem(time));
+}
