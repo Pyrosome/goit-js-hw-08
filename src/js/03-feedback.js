fb916dbdedf910337207ba110af6a13a953e741e
Gaultheria <valeriaburina@gmail.com>
2023-01-15 03:42:48 +0100

    task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,6 +3,7 @@ const form = document.querySelector('.feedback-form');
 
 onLoad();
 
+// input data collect and set to local storage ---------------//
 form.addEventListener('input', throttle(onInput, 500));
 
 function onInput() {
@@ -13,7 +14,7 @@ function onInput() {
 
   localStorage.setItem('feedback-form-state', JSON.stringify(formData));
 }
-
+// submit data to console and clear local storage ---------------//
 form.addEventListener('submit', e => {
   const formData = {
     email: form.email.value,
@@ -26,48 +27,17 @@ form.addEventListener('submit', e => {
   localStorage.removeItem('feedback-form-state');
 });
 
+// load data from local storage to form inputs ---------------//
 function onLoad() {
   const formInfo = localStorage.getItem('feedback-form-state');
   const parsedInfo = JSON.parse(formInfo);
-  form.email.value = parsedInfo.email;
-  form.message.value = parsedInfo.message;
+  if (parsedInfo) {
+    form.email.value = parsedInfo.email;
+    form.message.value = parsedInfo.message;
+  }
 }
 
-/*
+/* // //
 
-import throttle from 'lodash.throttle';
-const form = document.querySelector('.feedback-form');
-
-onLoad();
-
-form.addEventListener('input', throttle(onInput, 500));
-
-function onInput() {
-  const formData = {
-    email: form.email.value,
-    message: form.message.value
-  };
-
-  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
-}
-
-form.addEventListener('submit', (e) => {
-  const formData = {
-    email: form.email.value,
-    message: form.message.value
-  };
-  console.log(formData);
-
-  e.preventDefault();
-  form.reset();
-  localStorage.removeItem('feedback-form-state');
-});
-
-function onLoad() {
-  const formInfo = localStorage.getItem('feedback-form-state');
-  const parsedInfo = JSON.parse(formInfo);
-  form.email.value = parsedInfo.email;
-  form.message.value = parsedInfo.message;
-}
 
 */
